704d28af1a1c93dda2f1ee887ff6d2a4a1543391
mauro-ferrario <hello@mauroferrario.com>
2015-10-28 19:19:55 +0100

    Fix image size

--- src/SingleImagePage.cpp
+++ src/SingleImagePage.cpp
@@ -24,10 +24,21 @@ void SingleImagePage::draw()
   if(image.isAllocated())
   {
     ofRectangle imgCoords;
-    imgCoords.y = 0;
-    imgCoords.height = gallerySize.y;
-    imgCoords.width = float(image.width)/float(image.height)*float(imgCoords.height);
-    imgCoords.x = (gallerySize.x - imgCoords.width)*.5;
+    if(image.width < image.height)
+    {
+      imgCoords.y = 0;
+      imgCoords.height = gallerySize.y;
+      imgCoords.width = float(image.width)/float(image.height)*float(imgCoords.height);
+      imgCoords.x = (gallerySize.x - imgCoords.width)*.5;
+    }
+    else
+    {
+      imgCoords.x = 0;
+      imgCoords.width = gallerySize.x;
+      imgCoords.height = float(imgCoords.width)/(float(image.width)/float(image.height));
+      imgCoords.y = (gallerySize.y - imgCoords.height)*.5;
+    }
+    
     ofPushStyle();
     ofSetColor(255);
     image.draw(imgCoords);
